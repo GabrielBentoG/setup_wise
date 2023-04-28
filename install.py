a997581b3950d358c4e7d11dd8e7f0cde23f639d
GabrielBentoG <gabrielbentog0@gmail.com>
2023-04-28 16:20:34 -0300

    small changes

--- install.py
+++ install.py
@@ -2,7 +2,6 @@ import os
 import pathlib
 import subprocess
 
-
 def install():
     desktop_path = pathlib.Path.home() / "Desktop"
     setup_wise_path = desktop_path / "setup_wise"
@@ -10,9 +9,25 @@ def install():
         print(f"A pasta 'setup_wise' existe em {setup_wise_path}!")
     else:
         print("Não foi possível encontrar a pasta 'setup_wise'.")
+        return
 
     user_home = os.path.expanduser('~')
     setup_wise_path = os.path.join(user_home, 'Desktop', 'setup_wise')
-    filepath = os.path.join(setup_wise_path, 'download.exe')
-    process = subprocess.Popen(filepath)
-    process.communicate()
+
+    exe_files = [f for f in os.listdir(setup_wise_path) if f.endswith('.exe')]
+    if not exe_files:
+        print("Não foram encontrados arquivos executáveis na pasta 'setup_wise'.")
+        return
+
+    for filename in exe_files:
+        print(f"Instalando {filename}")
+        try:
+            filepath = os.path.join(setup_wise_path, filename)
+            process = subprocess.Popen(filepath)
+            return_code = process.wait()
+            if return_code == 0:
+                print(f"A instalação de {filename} foi concluída com sucesso.")
+            else:
+                print(f"Ocorreu um erro durante a instalação de {filename}.")
+        except Exception as e:
+            print(f"Ocorreu um erro durante a instalação de {filename}: {str(e)}")
--- setup_wise.py
+++ setup_wise.py
@@ -2,8 +2,8 @@ from download import download
 from install import install
 
 # Lista das URLs dos aplicativos para download
-app_urls = ['https://discordapp.com/api/download?platform=win', 
-            'https://download.scdn.co/SpotifySetup.exe']
+app_urls = [ 'https://download.scdn.co/SpotifySetup.exe',
+            'https://www.ccleaner.com/pt-br/ccleaner/download/standard']
             
 
 # Faz o download de cada aplicativo e salva dentro da pasta "setup_wise"
